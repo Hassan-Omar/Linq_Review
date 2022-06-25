9240d164836f4efe8898597fb2e096d6f4f8073b
Hassan Omar <49106965+Hassan-Omar@users.noreply.github.com>
2022-06-25 12:44:02 +0200

    Review Streaming, Excution

--- Program.cs
+++ Program.cs
@@ -14,26 +14,29 @@ namespace Linq_Review
             Console.WriteLine("________________Partitioning________________");
             foreach (var element in Partitioning.nums)
             {
-                Console.Write(element+"  ");
+                Console.Write(element + "  ");
             }
             Console.WriteLine("");
             Partitioning.TakeN(5);
-            Partitioning.TakeCriteria( x => x < 8); 
-            Partitioning.SkipN(2); 
-            Partitioning.SkipCriteria( x => x == 1); 
-            Console.ReadLine(); 
+            Partitioning.TakeCriteria(x => x < 8);
+            Partitioning.SkipN(2);
+            Partitioning.SkipCriteria(x => x == 1);
+            Console.WriteLine("________________Executions,Streaming________________");
+            Execution_Streaming.ExecutionExample();
+            Execution_Streaming.Stream_Or_Non_Example();
+            Console.ReadLine();
         }
     }
-    
+
     static class Partitioning
     {
-        public static int[] nums = { 4, 2, 3,1, 5, 6, 8, 7, 9, 10 };
+        public static int[] nums = { 4, 2, 3, 1, 5, 6, 8, 7, 9, 10 };
 
         public static void TakeN(int n)
         {
-            Console.WriteLine("Take({0})",n); 
+            Console.WriteLine("Take({0})", n);
             nums.Take(n).ToList().ForEach(item => {
-                Console.Write(item+" ");
+                Console.Write(item + " ");
             });
             Console.WriteLine("");
         }
@@ -54,12 +57,45 @@ namespace Linq_Review
             });
             Console.WriteLine("");
         }
-        public static void SkipCriteria(Func<int,bool> predicate)
+        public static void SkipCriteria(Func<int, bool> predicate)
         {
             Console.WriteLine("SkipCriteria(predicate)");
             nums.SkipWhile(predicate).ToList().ForEach(item => {
                 Console.Write(item + " ");
             });
+            Console.WriteLine("");
+        }
+    }
+
+    public static class Execution_Streaming
+    {
+        public static void ExecutionExample()
+        {
+            Console.WriteLine("nums.Count(), nums.ToList() both return {value} => they are Immediate Execution");
+            Console.WriteLine("nums.Select(), nums.Where() both return {IEnumerable<T> or IQueryable<T>} => they are Defred Execution");
+            Console.WriteLine("");
+        }
+        public static void Stream_Or_Non_Example()
+        {
+            Console.WriteLine("Stream Example => Select,Where,.....");
+            var lst = new List<int>() { 3, 5, 1, 2 };
+            var streamingQuery = lst.Select(x => {
+                Console.WriteLine(x);
+                return x;
+            });
+            foreach (var i in streamingQuery)
+            {
+                Console.WriteLine($"foreach iteration value: {i}");
+            }
+            Console.WriteLine("None Stream Example => OrderBy,GroupBy,.....");
+            var nonStreamingQuery = lst.OrderBy(x => {
+                Console.WriteLine(x);
+                return x;
+            });
+            foreach (var i in nonStreamingQuery)
+            {
+                Console.WriteLine($"foreach iteration value: {i}");
+            }
             Console.WriteLine("");
         }
     }
