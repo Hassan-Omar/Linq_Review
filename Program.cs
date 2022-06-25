4093762e2232d92376cca3164479bc5a2e08aa62
Hassan Omar <49106965+Hassan-Omar@users.noreply.github.com>
2022-06-25 18:37:24 +0200

    Review Grouping
    Join()
    GroupJoin()
    Zip()

--- Program.cs
+++ Program.cs
@@ -30,7 +30,13 @@ namespace Linq_Review
             Concatenation.Concate(a, b);
             Console.WriteLine("________________Filtering________________");
             Filtering.Where(Enumerable.Range(1,10).ToArray(), x => x>4);
-            Filtering.OfType(Enumerable.Range(1,10).ToArray());
+            Filtering.OfType(Enumerable.Range(1,10).ToArray()); 
+            Console.WriteLine("________________grouping________________");
+            Grouping grouping = new Grouping();
+            grouping.Init();
+            grouping.Join();
+            grouping.GroupJoin();
+            grouping.ZIP(); 
             Console.ReadLine();
         }
     }
@@ -140,5 +146,91 @@ namespace Linq_Review
             Console.WriteLine("");
         }
     }
+    public class Grouping
+    {
+        public class Student
+        {
+            public int ID = 1;
+            public int DepID = 1;
+            public string sName = "Student";
+        }
+        public class Depart
+        {
+            public int ID = 1; 
+            public string dName = "Depart";
+        }
 
+        public Depart[] Departs = new Depart[2];
+        public Student[] students = new Student[4];
+        public class Customer{
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+        public class Order{
+            public string Description { get; set; }
+            public int CustomerId { get; set; }
+        }
+
+        Customer[] customers = new Customer[]
+        {
+            new Customer { Id = 1, Name = "C1" },
+            new Customer { Id = 2, Name = "C2" },
+            new Customer { Id = 3, Name = "C3" }
+        };
+        Order[] orders = new Order[]
+        {
+            new Order { Description = "O1", CustomerId = 1 },
+            new Order { Description = "O2", CustomerId = 1 },
+            new Order { Description = "O3", CustomerId = 2 },
+            new Order { Description = "O4", CustomerId = 3 },
+        };
+        public void Init()
+        {
+            Depart d1 = new Depart();
+            Depart d2 = new Depart();
+            d1.ID = 1; d1.dName = "Math";
+            d2.ID = 2; d2.dName = "Physics";
+            Departs[0] = d1;
+            Departs[1] = d2; 
+
+            Student s1 = new Student(); 
+            Student s2 = new Student(); 
+            Student s3 = new Student(); 
+            Student s4 = new Student();
+            s1.ID = 1; s1.sName = "Moham"; s1.DepID = 1; 
+            s2.ID = 2; s2.sName = "HassN"; s2.DepID = 1;
+            s3.ID = 3; s3.sName = "Ali  "; s3.DepID = 1;
+            s4.ID = 4; s4.sName = "Abass"; s4.DepID = 2;
+            students[0] = s1;
+            students[1] = s2;
+            students[2] = s3;
+            students[3] = s4; 
+        }
+        public void Join()
+        {
+            Console.WriteLine("Join");
+            customers.Join(orders, c => c.Id, o => o.CustomerId, (c, o) => c.Name + "-" +
+                o.Description).ToList().ForEach(item =>
+                {
+                    Console.WriteLine(item);
+                });
+            Console.WriteLine("");
+        }
+        public void GroupJoin()
+        {
+            Console.WriteLine("GroupJoin");
+            customers.GroupJoin(orders, c => c.Id, o => o.CustomerId, (c, o) => c.Name).ToList().ForEach(item =>
+            {
+                Console.WriteLine(item);
+            }); ;
+            Console.WriteLine("");
+        }
+        public void ZIP()
+        {
+            Departs.Zip(students, (d,s) => new { d.dName, s.sName}).ToList().ForEach(item =>
+            {
+                Console.WriteLine(item.sName + "_" + item.dName);
+            }); 
+        }
+    }
 }
